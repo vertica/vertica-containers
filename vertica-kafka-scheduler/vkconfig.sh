00fade536c73560bb508bb417c7b0bc7b6e4b4bd
Duane Bronson <bronson@nerdlogic.com>
2022-11-02 17:16:42 -0400

    require dbhost and username
    the defaults for dbhost and username are different inside the container,
    so we need to explicity set them
    Also fixed fallback version code

--- vertica-kafka-scheduler/vkconfig.sh
+++ vertica-kafka-scheduler/vkconfig.sh
@@ -4,7 +4,7 @@
 # This script file wraps a containerized vkconfig.
 # Requires readlink but tries to make due without.
 
-: ${VERTICA_VERSION:=$(perl -nE 'my $v=$1 if m/Version\s*=\s*"v([\d\.]*)-/; END { say $v||"latest" }' /opt/vertica/sdk/BuildInfo.java 2>/dev/null) || echo "latest"}
+: ${VERTICA_VERSION:=$(perl -nE 'my $v=$1 if m/Version\s*=\s*"v([\d\.]*)-/; END { say $v||"latest" }' /opt/vertica/sdk/BuildInfo.java 2>/dev/null || echo "latest")}
 
 if ! SCRIPT_DIR=$(readlink -f ${BASH_SOURCE[0]} 2>/dev/null); then SCRIPT_DIR=${BASH_SOURCE[0]}; fi
 SCHED_DIR=${SCRIPT_DIR%/*/*} # take out /bin/vkconfig
@@ -19,15 +19,20 @@ declare -a vkconfigopts=("$1")
 shift
 # parse opts
 declare -a dockeropts
-conf=
-unset lastopt
+unset lastopt conf dbhost username
 for opt in $VKCONFIG_OPTS "$@"; do
   if [[ $opt =~ ^-- ]]; then
     lastopt="${opt}"
   elif [[ -n $lastopt ]]; then
     case "$lastopt" in
-      (--conf) 
-          conf=$opt     
+      (--username)
+          username=$opt
+      ;;
+      (--dbhost)
+          dbhost=$opt
+      ;;
+      (--conf)
+          conf=$opt
           opt=/etc/vkconfig.conf
       ;;
     esac
@@ -64,6 +69,24 @@ fi
 
 if [[ -r $conf ]]; then
   dockeropts+=( -v "$(abspath $conf):/etc/vkconfig.conf" )
+  # and look through $conf for some args that have a different meaning inside
+  # the continer so they must be specified.
+  set -e
+  source $conf
+  set +e
+fi
+
+# if dbhost isn't specified in the conf file or in the arguments, then it's
+# localhost and that won't work inside a container.
+if [[ -z $dbhost || localhost = $dbhost || 127.0.0.1 = $dbhost ]]; then
+  vkconfigopts+=( --dbhost host.docker.internal )
+  dockeropts+=( --add-host host.docker.internal:host-gateway )
+fi
+
+# if username isn't specified, then it takes the local username which is
+# different in the container, so we need to specify it explicitly.
+if [[ -z $username ]]; then
+  vkconfigopts+=( --username $(id -un) )
 fi
 
 : ${LOG_DIR:=/opt/vertica/log}
